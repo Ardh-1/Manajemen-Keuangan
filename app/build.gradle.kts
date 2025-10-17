152e8601ef3491ef9dd395d7af2e01da68687846
Ardh <adiardian246@gmail.com>
2025-10-17 09:41:03 +0700

    v1.1.2 - menurunkan minimal SDK 12 -> 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.arwil.mk"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
